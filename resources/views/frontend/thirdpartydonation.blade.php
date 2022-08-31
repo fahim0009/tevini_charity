a326236df7d082b5481c17515a8ba9d0517b0fea
hasib2019 <hasib.9437.hu@gmail.com>
2022-08-31 22:18:06 +0600

    api variable change

--- resources/views/frontend/thirdpartydonation.blade.php
+++ resources/views/frontend/thirdpartydonation.blade.php
@@ -85,17 +85,17 @@
     }
 </style>
         @php
-            if(isset($_GET["tevini_campaignid"]) && isset($_GET["acc"]) && isset($_GET["amt"]) && isset($_GET["hash"])) {
-               $tevini_campaignid = $_GET["tevini_campaignid"];
+            if(isset($_GET["campaignid"]) && isset($_GET["acc"]) && isset($_GET["amt"]) && isset($_GET["hash"])) {
+               $campaignid = $_GET["campaignid"];
                 $transid = $_GET["transid"];
                 $acc = $_GET["acc"];
                 $amt = $_GET["amt"];
                 $comment = $_GET["comment"];
                 $charidy_hash = $_GET["hash"];
 
-                $campaign_dtls =\App\Models\Campaign::where('id',$tevini_campaignid)->first();
+                $campaign_dtls =\App\Models\Campaign::where('id',$campaignid)->first();
                 if(!empty($campaign_dtls)){
-                $mhash = "?tevini_campaignid=".$tevini_campaignid."&transid=".$transid."&acc=".$acc."&amt=".$amt;
+                $mhash = "?campaignid=".$campaignid."&transid=".$transid."&acc=".$acc."&amt=".$amt;
                 $tevini_hash = hash_hmac("sha256", $mhash, $campaign_dtls->hash_code);
                 }
             // echo $mhash."<br>";
@@ -130,10 +130,10 @@
                     <div class="px-3 pb-5">
                         <h2 class=" intro mb-0 text-white">
                             You are now completing your
-                            donation to  {{\App\Models\Campaign::where('id',$tevini_campaignid)->first()->campaign_title}} using funds in your
+                            donation to  {{\App\Models\Campaign::where('id',$campaignid)->first()->campaign_title}} using funds in your
                             Tevini account.
                         </h2>
-                        <h5 class="mt-3 tagline text-white">Charity: {{\App\Models\Charity::where('id',\App\Models\Campaign::where('id',$tevini_campaignid)->first()->charity_id)->first()->name}}</h4>
+                        <h5 class="mt-3 tagline text-white">Charity: {{\App\Models\Charity::where('id',\App\Models\Campaign::where('id',$campaignid)->first()->charity_id)->first()->name}}</h4>
                         <img src="{{ asset('assets/image/arrow.png') }}" class="arrow" alt="">
                     </div>
                 </div>
@@ -168,7 +168,7 @@
                                 Please allow up to one week for your donation to be processed.
                             </small>
 
-                            <input type="text" hidden id="tevini_campaignid" value="{{$tevini_campaignid}}">
+                            <input type="text" hidden id="campaignid" value="{{$campaignid}}">
                             <input type="text" hidden id="transid" value="{{$transid}}">
                             <input type="text" hidden id="comment" value="{{$comment}}">
                             <input type="text" hidden id="hash" value="{{$charidy_hash}}">
@@ -216,7 +216,7 @@
                     var transid= $("#transid").val();
                     var acc= $("#acc").val();
                     var amt= $("#amt").val();
-                    var tevini_campaignid= $("#tevini_campaignid").val();
+                    var campaignid= $("#campaignid").val();
                     var comment= $("#comment").val();
                     var password= $("#password").val();
                     var hash= $("#hash").val();
@@ -224,7 +224,7 @@
                     $.ajax({
                         url: url,
                         method: "POST",
-                        data: {transid,acc,amt,tevini_campaignid,comment,hash,password},
+                        data: {transid,acc,amt,campaignid,comment,hash,password},
                         success: function (d) {
 
                             if (d.status == 303) {

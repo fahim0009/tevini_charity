b4ae6982193555f9d872e4a4a9bf8367c1d00e36
fahim0009 <fahim.amin71@gmail.com>
2023-04-07 15:20:54 +0600

    waiting mail

--- resources/views/mail/waiting_voucher.blade.php
+++ resources/views/mail/waiting_voucher.blade.php
@@ -3,11 +3,16 @@
 
 <h2>Dear {{ $array['donor']->name}},</h2>
 
-<p>The waiting vouchers listed in the attached file. Please check and confirm.</p>
+<p>The waiting vouchers are listed in the attached file. Please check and confirm.</p>
 
-@component('mail::button', ['url' => url('/user/process-voucher')])
+<p>To confirm, please login to your dashboard and click on "Waiting Voucher" on the left side.</p>
+
+
+@component('mail::button', ['url' => url('https://www.tevini.co.uk/user/process-voucher')])
 Click here
 @endcomponent
+
+
 
 Tevini
 

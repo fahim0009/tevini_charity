efccb9cbcf386e03994ec856e2934dacbe56c29c
fahim0009 <fahim.amin71@gmail.com>
2023-05-11 13:34:43 +0600

    donor report mail solve

--- resources/views/mail/donorreport.blade.php
+++ resources/views/mail/donorreport.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 
-<h3>Dear {{$array['bname']}},</h3>
+<h3>Dear {{$array['name']}},</h3>
 
 <p>Please find the attached statement.</p>
 <p>if your balance is in debit please make a payment to top up your account.</p>

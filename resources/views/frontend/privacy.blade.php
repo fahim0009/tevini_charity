aa30885c8bb4b164337619a00c30b42e8329c7d9
mshakil5 <kazimuhammadullah@gmail.com>
2023-08-11 23:52:20 +0600

    show pin

--- resources/views/frontend/privacy.blade.php
+++ resources/views/frontend/privacy.blade.php
@@ -25,23 +25,24 @@
 
 <div class="container">
 
+    <p style="text-indent: 0pt; text-align: left;"><br /></p>
+<h1 style="padding-top: 1pt; padding-left: 6pt; text-indent: 0pt; text-align: left; font-size:32px">Privacy Policy for Tevini Ltd Website</h1>
 
-
-<p class="s1" style="padding-top: 1pt; padding-left: 5pt; text-indent: 0pt; text-align: left;">Privacy Policy for Tevini Ltd Website</p>
 <p style="text-indent: 0pt; text-align: left;"><br /></p>
 <p style="padding-top: 12pt; padding-left: 5pt; text-indent: 0pt; text-align: left;">Effective Date: 10/08/2023</p>
+
 <p style="text-indent: 0pt; text-align: left;"><br /></p>
 <p style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
     At <b>Tevini Ltd</b>, we are committed to protecting and respecting your privacy. This Privacy Policy outlines how we collect, use, disclose, and protect the information you provide to us when you visit our website or interact with our
     online services. Please take a moment to read this Privacy Policy to understand our practices.
 </p>
-<p style="text-indent: 0pt; text-align: left;"><br /></p>
+
 <ol id="l1">
     <li data-list-text="1.">
         <h1 style="padding-left: 15pt; text-indent: -10pt; text-align: left;">Information We Collect</h1>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <p style="padding-left: 5pt; text-indent: 0pt; text-align: left;">We may collect the following types of information:</p>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <ol id="l2">
             <li data-list-text="1.1">
                 <h1 style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
@@ -51,7 +52,7 @@
                         with our website.
                     </span>
                 </h1>
-                <p style="text-indent: 0pt; text-align: left;"><br /></p>
+                
             </li>
             <li data-list-text="1.2">
                 <h1 style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
@@ -61,21 +62,21 @@
                         this information.
                     </span>
                 </h1>
-                <p style="text-indent: 0pt; text-align: left;"><br /></p>
+                
             </li>
         </ol>
     </li>
     <li data-list-text="2.">
         <h1 style="padding-left: 15pt; text-indent: -10pt; text-align: left;">How We Use Your Information</h1>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <p style="padding-left: 5pt; text-indent: 0pt; text-align: left;">We use the information we collect for various purposes, including:</p>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <ol id="l3">
             <li data-list-text="2.1">
                 <h1 style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
                     Providing Services<span class="p">: To deliver the services you request from us, such as responding to inquiries, processing orders, and providing customer support.</span>
                 </h1>
-                <p style="text-indent: 0pt; text-align: left;"><br /></p>
+                
             </li>
             <li data-list-text="2.2">
                 <h1 style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
@@ -86,21 +87,21 @@
                 <h1 style="padding-top: 1pt; padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
                     Improving Website<span class="p">: To analyze usage patterns, improve our website, and customize content based on your preferences.</span>
                 </h1>
-                <p style="text-indent: 0pt; text-align: left;"><br /></p>
+                
             </li>
             <li data-list-text="2.4">
                 <h1 style="padding-left: 5pt; text-indent: 0pt; line-height: 107%; text-align: left;">Legal Compliance<span class="p">: To comply with legal obligations, resolve disputes, and enforce our terms and conditions.</span></h1>
-                <p style="text-indent: 0pt; text-align: left;"><br /></p>
+                
             </li>
         </ol>
     </li>
     <li data-list-text="3.">
         <h1 style="padding-left: 15pt; text-indent: -10pt; text-align: left;">Disclosure of Your Information</h1>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <p style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
             We do not sell, trade, or rent your personal information to third parties. However, we may share your information with trusted third parties under the following circumstances:
         </p>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <ol id="l4">
             <li data-list-text="3.1">
                 <h1 style="padding-left: 5pt; text-indent: 0pt; line-height: 107%; text-align: left;">
@@ -108,61 +109,61 @@
                     <span class="p">: We may engage third-party service providers to assist with our operations, such as website hosting, payment processing, and analytics. These providers have access to your information</span>
                 </h1>
                 <p style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">only as needed to perform their tasks on our behalf and are obligated not to disclose or use it for other purposes.</p>
-                <p style="text-indent: 0pt; text-align: left;"><br /></p>
+                
             </li>
             <li data-list-text="3.2">
                 <h1 style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">Legal Requirements<span class="p">: We may disclose your information when required by law, court order, or government request.</span></h1>
-                <p style="text-indent: 0pt; text-align: left;"><br /></p>
+                
             </li>
             <li data-list-text="3.3">
                 <h1 style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
                     Business Transfers<span class="p">: In the event of a merger, acquisition, or sale of assets, your information may be transferred to the acquiring entity.</span>
                 </h1>
-                <p style="text-indent: 0pt; text-align: left;"><br /></p>
+                
             </li>
         </ol>
     </li>
     <li data-list-text="4.">
         <h1 style="padding-left: 15pt; text-indent: -10pt; text-align: left;">Data Security</h1>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <p style="padding-left: 5pt; text-indent: 0pt; text-align: left;">We take appropriate measures to protect your information from unauthorized access, alteration,</p>
         <p style="padding-top: 1pt; padding-left: 5pt; text-indent: 0pt; line-height: 107%; text-align: left;">
             disclosure, or destruction. However, no data transmission over the internet can be guaranteed as 100% secure. You acknowledge that you provide your information at your own risk.
         </p>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
     </li>
     <li data-list-text="5.">
         <h1 style="padding-left: 15pt; text-indent: -10pt; text-align: left;">Your Rights</h1>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <p style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
             You have certain rights concerning your personal information, including the right to access, correct, and delete your data. You can also opt out of receiving marketing communications from us.
         </p>
     </li>
     <li data-list-text="6.">
         <h1 style="padding-top: 1pt; padding-left: 15pt; text-indent: -10pt; text-align: left;">Third-Party Links</h1>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <p style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
             Our website may contain links to third-party websites. We are not responsible for the privacy practices or content of these websites. We encourage you to review the privacy policies of any third-party sites you visit.
         </p>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
     </li>
     <li data-list-text="7.">
         <h1 style="padding-left: 15pt; text-indent: -10pt; text-align: left;">Changes to this Privacy Policy</h1>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
         <p style="padding-left: 5pt; text-indent: 0pt; text-align: left;">We may update this Privacy Policy from time to time to reflect changes in our practices or for other</p>
         <p style="padding-top: 1pt; padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">operational, legal, or regulatory reasons. We will notify you of any changes by posting the updated policy on our website.</p>
-        <p style="text-indent: 0pt; text-align: left;"><br /></p>
+        
     </li>
     <li data-list-text="8."><h1 style="padding-left: 15pt; text-indent: -10pt; text-align: left;">Contact Us</h1></li>
 </ol>
-<p style="text-indent: 0pt; text-align: left;"><br /></p>
+
 <p style="padding-left: 5pt; text-indent: 0pt; line-height: 108%; text-align: left;">
     If you have any questions, concerns, or requests regarding this Privacy Policy or your personal information, please contact us at 
     <a href="mailto:info@tevini.co.uk" target="_blank">info@tevini.co.uk</a>
 </p>
-<p style="text-indent: 0pt; text-align: left;"><br /></p>
-<p style="padding-left: 5pt; text-indent: 0pt; text-align: left;">By using our website, you consent to the practices described in this Privacy Policy.</p>
 
+<p style="padding-left: 5pt; text-indent: 0pt; text-align: left;">By using our website, you consent to the practices described in this Privacy Policy.</p>
+<p style="text-indent: 0pt; text-align: left;"><br /></p>
 
 
 </div>

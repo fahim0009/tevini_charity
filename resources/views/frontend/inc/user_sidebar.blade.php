4e456be655f848b6ec0a2260ae47c52c8f5497a4
shakil <kazimuhammadullah@gmail.com>
2024-02-11 03:15:21 +0600

    food

--- resources/views/frontend/inc/user_sidebar.blade.php
+++ resources/views/frontend/inc/user_sidebar.blade.php
@@ -5,22 +5,29 @@
     <ul class="navigation">
 
         <li><a href="{{ route('user.dashboard') }}" class="{{ (request()->is('user/dashboard*')) ? 'nav-link current' : '' }}">Dashboard</a></li>
-        <li><a href="{{ route('user.makedonation') }}" class="{{ (request()->is('user/make-donation*')) ? 'nav-link current' : '' }}">Make a donation</a></li>
-        <li><a href="{{ route('user.donationrecord') }}" class="{{ (request()->is('user/donation-record*')) ? 'nav-link current' : '' }}">Donation records</a></li>
-        <li><a href="{{ route('user.standingrecord') }}" class="{{ (request()->is('user/standing-order-record*')) ? 'nav-link current' : '' }}">Standing order records</a></li>
-        <li><a href="{{ route('user.orderbook') }}" class="{{ (request()->is('user/order-voucher-book*')) ? 'nav-link current' : '' }}">Order voucher books</a></li>
-        <li><a href="{{ route('user.orderhistory') }}" class="{{ (request()->is('user/order-history*')) ? 'nav-link current' : '' }}">Order Record</a></li>
-        <li><a href="{{ route('user.process_voucher') }}" class="{{ (request()->is('user/process-voucher*')) ? 'nav-link current' : '' }}">Waiting vouchers</a></li>
-        <li><a href="{{ route('userCardService')}}" class="{{ (request()->is('user/card-service*')) ? 'nav-link current' : '' }}">Tevini card</a></li>
-        <li><a href="{{ route('user.transaction') }}" class="{{ (request()->is('user/transaction-view*')) ? 'nav-link current' : '' }}">View transactions</a></li>
-        <li><a href="{{ route('user.donationcal') }}" class="{{ (request()->is('user/donation-calculation*')) ? 'nav-link current' : '' }}">Maaser calculator</a></li>
-        <li><a href="{{ route('user.contact') }}" class="{{ (request()->is('user/contact*')) ? 'nav-link current' : '' }}">Contact/Top-Up</a></li>
+
+        @if (Auth::user()->accountno)
+            <li><a href="{{ route('user.makedonation') }}" class="{{ (request()->is('user/make-donation*')) ? 'nav-link current' : '' }}">Make a donation</a></li>
+            <li><a href="{{ route('user.donationrecord') }}" class="{{ (request()->is('user/donation-record*')) ? 'nav-link current' : '' }}">Donation records</a></li>
+            <li><a href="{{ route('user.standingrecord') }}" class="{{ (request()->is('user/standing-order-record*')) ? 'nav-link current' : '' }}">Standing order records</a></li>
+            <li><a href="{{ route('user.orderbook') }}" class="{{ (request()->is('user/order-voucher-book*')) ? 'nav-link current' : '' }}">Order voucher books</a></li>
+            <li><a href="{{ route('user.orderhistory') }}" class="{{ (request()->is('user/order-history*')) ? 'nav-link current' : '' }}">Order Record</a></li>
+            <li><a href="{{ route('user.process_voucher') }}" class="{{ (request()->is('user/process-voucher*')) ? 'nav-link current' : '' }}">Waiting vouchers</a></li>
+            <li><a href="{{ route('userCardService')}}" class="{{ (request()->is('user/card-service*')) ? 'nav-link current' : '' }}">Tevini card</a></li>
+            <li><a href="{{ route('user.transaction') }}" class="{{ (request()->is('user/transaction-view*')) ? 'nav-link current' : '' }}">View transactions</a></li>
+            <li><a href="{{ route('user.donationcal') }}" class="{{ (request()->is('user/donation-calculation*')) ? 'nav-link current' : '' }}">Maaser calculator</a></li>
+            <li><a href="{{ route('user.contact') }}" class="{{ (request()->is('user/contact*')) ? 'nav-link current' : '' }}">Contact/Top-Up</a></li>
+        @endif
+        
         
         {{-- <li><a href="{{ route('userCardService') }}" class="{{ (request()->is('user/card-service*')) ? 'nav-link current' : '' }}">Card Service</a></li> --}}
     </ul>
     <div class="bottom-part">
+        @if (Auth::user()->accountno)
         <a href="{{ route('user.orderbook') }}" class="btn-theme bg-secondary">Order voucher books</a>
         <a href="{{ route('user.makedonation')}}" class="btn-theme bg-primary">Make a donation</a>
+        @endif
+
         <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="mt-2 d-flex justify-content-center txt-theme fw-bold align-items-center">
             <iconify-icon icon="humbleicons:logout"></iconify-icon>
             &nbsp;Log out
--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -52,16 +52,19 @@ use Illuminate\Support\Carbon;
             <div class="col-lg-6 ">
                 <img src="{{ asset('assets/user/images/card.png') }}" class="img-fluid mt-3 mb-2" alt="">
             </div>
-
+            @if (Auth::user()->accountno)
             <div class="col-lg-6  pt-3 d-flex flex-column px-4">
+                
                 <a href="{{ route('user.makedonation') }}" class="btn-theme bg-primary">Make a
                     donation</a>
                 <a href="{{ route('user.orderbook') }}" class="btn-theme bg-secondary">Order voucher books</a>
                 <a href="{{ route('stripeDonation')}}" class="btn-theme bg-ternary">Top up account</a>
+                
             </div>
             <div class="col-lg-12">
                 <a href="{{ route('userCardService')}}" class="btn-theme bg-primary d-block fs-14 txt-theme fw-bold">Order a card</a>
             </div>
+            @endif
         </div>
         <div class="  p-4 py-5 mt-2" style="background-color: #D9D9D9;">
             <div>

f87afc38a07d5677e1eaa802bc0cbd1fa6416d50
shakil <kazimuhammadullah@gmail.com>
2024-03-07 00:13:12 +0600

    tevini tdf transaction

--- resources/views/mail/charitypayment.blade.php
+++ resources/views/mail/charitypayment.blade.php
@@ -3,8 +3,7 @@
 <h3>Dear {{$array['name']}},</h3>
 
 <p>We have made a payment of £{{$array['amount']}} transaction ID {{$array['t_id']}} with the following notes "{{$array['note']}}"</p>
-<p>Please kindly send a receipt of funds to this email.</p>
-
+<p>For more details on this payment please visit your charity's online portal here <a href="https://www.tevini.co.uk/charity_login">Tevini Ligin</a>  for login details please contact us.</p>
 
 Thanks,<br>
 {{ config('app.name') }}

0c5a5ec1b906bbdd1d41ace50f43886f904af74b
shakil <kazimuhammadullah@gmail.com>
2024-07-18 13:27:50 +0600

    make a donation api test

--- app/Http/Controllers/DonorController.php
+++ app/Http/Controllers/DonorController.php
@@ -566,7 +566,7 @@ class DonorController extends Controller
 
     public function userDonationStore(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
         $userid = $request->userid;
 
         if(empty($request->charity_id)){

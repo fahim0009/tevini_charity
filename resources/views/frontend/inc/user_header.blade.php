71c42cf54dd1bd27befff4a10739ba5a7de96762
shakil <kazimuhammadullah@gmail.com>
2025-04-06 12:05:52 +0600

    header

--- resources/views/frontend/inc/user_header.blade.php
+++ resources/views/frontend/inc/user_header.blade.php
@@ -4,7 +4,7 @@
             <iconify-icon class="icon" icon="ic:baseline-search"></iconify-icon>
             <input type="text" class="inputSearch" placeholder="Search">
         </label>
-        <div class="txt-theme fs-16">Account Number: <span class="fw-bold">{{auth()->user()->accountno}}</span> </div>
+        <div class="txt-theme fs-16">Account Number: <span class="fw-bold">{{auth()->user()->accountno ?? ''}}</span> </div>
     </div>
     <div class="items position-relative d-flex justify-content-end align-items-center">
         <div class="dropdown account">

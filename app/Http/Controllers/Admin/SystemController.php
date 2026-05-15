4646d2e893c35cfdc1c786597c3a8f6874762586
shakil <46122677+mshakil5@users.noreply.github.com>
2026-05-15 15:41:54 +0600

    transaction issue

--- app/Http/Controllers/Admin/SystemController.php
+++ app/Http/Controllers/Admin/SystemController.php
@@ -23,7 +23,8 @@ class SystemController extends Controller
             ->where('status', 1)
             ->where('t_type', 'Out')
             ->where('name', '=', 'Bank')
-            ->whereTime('created_at', '=', '17:29:00');
+            ->whereRaw('TIME(created_at) BETWEEN ? AND ?', ['17:20:00', '17:39:59']);
+            // ->whereTime('created_at', '=', '17:29:00');
 
             // ->whereRaw('TIME(created_at) BETWEEN ? AND ?', ['17:00:00', '17:59:59'])
  
